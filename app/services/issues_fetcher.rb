0a0962b2a2df5f4c4b461870e49a35c876ea1907
Mohnish G J <mail4mohnish@gmail.com>
2025-08-04 11:23:48 +0200

    Updated the Issue Fetcher sort options & use fragment caching
    - We use fragment caching with a cache version(cv)
    - Also set default sort to be based on created_at & based on open issues

--- app/services/issues_fetcher.rb
+++ app/services/issues_fetcher.rb
@@ -1,35 +1,53 @@
 # frozen_string_literal: true
 
 class IssuesFetcher
-  # Allows using Pagy in a Service class
   include Pagy::Backend
 
-  attr_reader :repository, :status, :page, :per_page
+  attr_reader :repository, :state, :page, :per_page, :cv
 
   def initialize(repository, params = {})
     @repository = repository
-    @status = params[:status] || "all"
+    # Support both '' and 'state' parameters for backward compatibility
+    @state = params[:state] || "open"
     @page = params[:page] || 1
-    @per_page = params[:per_page] || Pagy::DEFAULT[:items]
+    @per_page = params[:per_page].present? ? params[:per_page].to_i : Pagy::DEFAULT[:items]
+    @cv = params[:cv] || calculate_current_cv
   end
 
-  def fetch_from_database
-    scope = build_query_scope
-    pagy, issues = pagy(scope, items: per_page, page:)
-
-    {
-      issues:,
-      total_count: pagy.count,
-      pagy:
-    }
+  def fetch
+    Rails.cache.fetch(cache_key_with_cv, expires_in: 1.year) do
+      fetch_from_database
+    end
   end
 
   private
+    def calculate_current_cv
+      # Use only issue_updated_at for cache versioning
+      # New issues have same created/updated timestamps
+      GithubIssue.by_repository(repository).maximum(:issue_updated_at)&.to_i || 0
+    end
+
+    def fetch_from_database
+      scope = build_query_scope
+      pagy, issues = pagy(scope, items: per_page, page:)
+
+      {
+        issues:,
+        total_count: pagy.count,
+        pagy:,
+        per_page:
+      }
+    end
+
     def build_query_scope
       GithubIssue.includes(:github_user)
            .by_repository(repository)
-           .by_state(status)
-           .order(issue_updated_at: :desc)
+           .by_state(state)
+           .order(issue_created_at: :desc)
+    end
+
+    def cache_key_with_cv
+      "issues:#{repository}:#{state}:page_#{page}:per_page_#{per_page}:cv_#{cv}"
     end
 
     # Required by Pagy::Backend
